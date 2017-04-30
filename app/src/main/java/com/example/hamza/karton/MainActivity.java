31ba71d09ede7ba971ca95f49221659cb78e9dce
Hamza <hamysam6307@gmail.com>
2017-05-01 01:04:02 +0500

    chanfes

--- app/src/main/java/com/example/hamza/karton/MainActivity.java
+++ app/src/main/java/com/example/hamza/karton/MainActivity.java
@@ -1,13 +1,82 @@
 package com.example.hamza.karton;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+  // declaration here.........
+    Toolbar toolbar;
+    DrawerLayout drawer;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+    Fragment fragment;
+    FragmentManager fragmentManager;
+  // on back press nevigation drawer
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      // initilization.....
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        setSupportActionBar(toolbar);
+
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
+    }
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        Fragment fragment = null;
+        switch (id) {
+            case R.id.nav_utube:
+                fragment = new Fragment_utube_activity();
+                break;
+            case R.id.nav_Calculater:
+                fragment = new Frafment_Calculater_activity();
+                break;
+            case R.id.nav_picker:
+                fragment = new colorPicker_activity();
+                break;
+
+            case R.id.nav_Facebook:
+                fragment = new facebook_activity();
+                break;
+        }
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.flContent, fragment)
+                .commit();
+
+        navigationView.setCheckedItem(id);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
+
