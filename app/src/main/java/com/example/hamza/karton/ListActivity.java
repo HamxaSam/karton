bc09ee5366f7ff25fe39bafeabf09a3fcab5dbdd
Hamza <hamysam6307@gmail.com>
2017-05-02 15:16:27 +0500

    combing work nevigation drawer and recycler view

--- app/src/main/java/com/example/hamza/karton/ListActivity.java
+++ app/src/main/java/com/example/hamza/karton/ListActivity.java
@@ -1,17 +1,25 @@
 package com.example.hamza.karton;
 
+import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class ListActivity extends AppCompatActivity {
+public class ListActivity extends Fragment {
  ProgressDialog pDialog;
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
-        pDialog = new ProgressDialog(ListActivity.this);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+       View v = inflater.inflate(R.layout.activity_list,container,false);
+
+       pDialog = new ProgressDialog(getActivity().getApplication());
         pDialog.setMessage("Please wait ...");
 
     }
 }
+
